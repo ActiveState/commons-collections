8c83233da2cf30f5b89b80754bbea8f775555a0f
Thomas Neidhart <tn@apache.org>
2015-11-09 21:09:05 +0000

    [COLLECTIONS-580] Add javadoc, improve error message and apply review comments.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/collections/branches/COLLECTIONS_3_2_X@1713537 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/functors/InvokerTransformer.java
+++ src/java/org/apache/commons/collections/functors/InvokerTransformer.java
@@ -22,6 +22,8 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
 import org.apache.commons.collections.FunctorException;
 import org.apache.commons.collections.Transformer;
@@ -30,13 +32,14 @@ import org.apache.commons.collections.Transformer;
  * Transformer implementation that creates a new object instance by reflection.
  * <p>
  * <b>WARNING:</b> from v3.2.2 onwards this class will throw an
- * {@link UnsupportedOperationException} when trying to serialize or
- * de-serialize an instance to prevent potential remote code execution exploits.
+ * {@link UnsupportedOperationException} when trying to de-serialize an
+ * instance from a {@link ObjectOutputStream} to prevent potential
+ * remote code execution exploits.
  * <p>
- * In order to re-enable serialization support for {@code InvokerTransformer}
- * the following system property can be used (via -Dproperty=true):
+ * In order to re-enable de-serialization of {@code InvokerTransformer}
+ * instances, the following system property can be used (via -Dproperty=true):
  * <pre>
- * org.apache.commons.collections.enableUnsafeSerialization
+ * org.apache.commons.collections.invokertransformer.enableDeserialization
  * </pre>
  * 
  * @since Commons Collections 3.0
@@ -48,6 +51,10 @@ public class InvokerTransformer implements Transformer, Serializable {
 
     /** The serial version */
     private static final long serialVersionUID = -8653385846894047688L;
+
+    /** System property key to enable de-serialization */
+    public final static String DESERIALIZE
+        = "org.apache.commons.collections.invokertransformer.enableDeserialization";
 
     /** The method name to call */
     private final String iMethodName;
@@ -148,20 +155,29 @@ public class InvokerTransformer implements Transformer, Serializable {
     }
 
     /**
-     * Overrides the default writeObject implementation to prevent
-     * serialization (see COLLECTIONS-580).
-     */
-    private void writeObject(ObjectOutputStream os) throws IOException {
-        FunctorUtils.checkUnsafeSerialization(InvokerTransformer.class);
-        os.defaultWriteObject();
-    }
-
-    /**
      * Overrides the default readObject implementation to prevent
      * de-serialization (see COLLECTIONS-580).
      */
     private void readObject(ObjectInputStream is) throws ClassNotFoundException, IOException {
-        FunctorUtils.checkUnsafeSerialization(InvokerTransformer.class);
+        String deserializeProperty;
+        
+        try {
+            deserializeProperty = 
+                (String) AccessController.doPrivileged(new PrivilegedAction() {
+                    public Object run() {
+                        return System.getProperty(DESERIALIZE);
+                    }
+                });
+        } catch (SecurityException ex) {
+            deserializeProperty = null;
+        }
+
+        if (!"true".equalsIgnoreCase(deserializeProperty)) {
+            throw new UnsupportedOperationException(
+                    "Deserialization of InvokerTransformer is disabled for security reasons. " +
+                    "To re-enable it set system property '" + DESERIALIZE + "' to 'true'");
+        }
+        
         is.defaultReadObject();
     }
 }
--- src/test/org/apache/commons/collections/functors/TestInvokerTransformer.java
+++ src/test/org/apache/commons/collections/functors/TestInvokerTransformer.java
@@ -1,25 +1,18 @@
-/*
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package org.apache.commons.collections.functors;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.apache.commons.collections.BulkTest;
+
+import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-public class TestInvokerTransformer extends AbstractTestSerialization {
+public class TestInvokerTransformer extends BulkTest {
 
     // conventional
     // ------------------------------------------------------------------------
@@ -34,12 +27,53 @@ public class TestInvokerTransformer extends AbstractTestSerialization {
 
     // ------------------------------------------------------------------------
 
-    public Object makeObject() {
-        return new InvokerTransformer("toString", new Class[0], new Object[0]);
+    public void testSerializationDisabled() throws Exception {
+        Assert.assertNull(System.getProperty(InvokerTransformer.DESERIALIZE));
+        InvokerTransformer transformer = new InvokerTransformer("toString", new Class[0], new Object[0]);
+        byte[] data = serialize(transformer);
+        Assert.assertNotNull(data);
+        try {
+            deserialize(data);
+            fail("de-serialization of InvokerTransformer should be disabled by default");
+        } catch (UnsupportedOperationException ex) {
+            // expected
+        }
     }
 
-    public Class getTestClass() {
-        return InvokerTransformer.class;
+    public void testSerializationEnabled() throws Exception {
+        Assert.assertNull(System.getProperty(InvokerTransformer.DESERIALIZE));
+        System.setProperty(InvokerTransformer.DESERIALIZE, "true");
+
+        try {
+            InvokerTransformer transformer = new InvokerTransformer("toString", new Class[0], new Object[0]);
+            byte[] data = serialize(transformer);
+            Assert.assertNotNull(data);
+            try {
+                Object obj = deserialize(data);
+                Assert.assertTrue(obj instanceof InvokerTransformer);
+            } catch (UnsupportedOperationException ex) {
+                fail("de-serialization of InvokerTransformer should be enabled");
+            }
+        } finally {
+            System.clearProperty(InvokerTransformer.DESERIALIZE);
+        }
+    }
+    
+    private byte[] serialize(InvokerTransformer transformer) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+
+        oos.writeObject(transformer);
+        oos.close();
+
+        return baos.toByteArray();
+    }
+    
+    private Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(data);
+        ObjectInputStream iis = new ObjectInputStream(bais);
+        
+        return iis.readObject();
     }
 
 }
